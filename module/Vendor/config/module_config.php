caa82470f853d82bac5133ff1f1ad9ab048b02f0
meenasubramanian86 <me.smeena@gmail.com>
2016-03-05 11:55:00 +0530

    Mar 05

---
+++ module/Vendor/config/module_config.php
@@ -0,0 +1,57 @@
+<?php
+return array(
+
+    'service_manager' => array(
+        'factories' => array(
+           'Vendor\Service\VendorServiceInterface' => 'Vendor\Factory\VendorServiceFactory',
+           'Vendor\Mapper\VendorMapperInterface' => 'Vendor\Factory\VendorMapperFactory',
+        ),
+    ),
+
+    'controllers' => array(
+        'factories' => array(
+            'Vendor\Controller\Vendor' => 'Vendor\Factory\VendorControllerFactory',
+        ),
+    ),
+	
+    'router' => array(
+        'routes' => array(
+	 
+                'createvendor' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/vendor',
+                    'defaults' => array(
+                        'controller' => 'Vendor\Controller\Vendor',
+                        'action' => 'addVendor',
+                    ),
+                ),
+            ),
+			
+			'candidateLogin' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/candidateLogin',
+                    'defaults' => array(
+                        'controller' => 'Vendor\Controller\Vendor',
+                        'action' => 'candidateLogin',
+                    ),
+                ),
+            ),
+            
+            
+            
+ 			
+			
+        ),
+    ),
+
+    
+	'view_manager'	=> array(
+        'strategies' => array(
+            'ViewJsonStrategy',
+        ),
+	),
+	
+	
+);
---
+++ module/Vendor/src/Vendor/Model/VendorInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vendor\Model;
+
+interface VendorInterface
+{
+	
+	public function getNumberOfVendor();
+	public function setNumberOfVendor($numberOfVendor);
+	 
+	public function getVendorId();
+	public function setVendorId($vendorId);
+	 
+	public function loginResult();
+ 		
+}
